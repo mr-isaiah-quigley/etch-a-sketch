e3bb70a918428ad8217faaa94d51788ecf77fa7a
mr-isaiah-quigley <mr.isaiah.quigley@gmail.com>
2024-05-23 21:35:51 -0400

    create rough sketch of etch-a-sketch board

--- script.js
+++ script.js
@@ -3,3 +3,12 @@
     Copyright 2024 Isaiah Quigley
     Completed as part of the course work for The Odin Project
 */
+
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16**2; i++) {
+    const etchGrid = document.createElement("div");
+    etchGrid.classList.add("etchGrid");
+    etchGrid.textContent = "test";
+    container.appendChild(etchGrid);
+}
